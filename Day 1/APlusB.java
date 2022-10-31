09781f0ba580ba967979ad9ddff8d17829180c2e
eugene <garden8b103@gmail.com>
2022-10-31 12:46:44 +0300

    add first day

---
+++ Day 1/APlusB.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+/*
+Требуется сложить два целых числа А и В.
+
+Входные данные:
+В единственной строке входного файла INPUT.TXT записаны два натуральных числа через пробел.
+Значения чисел не превышают 10^9.
+
+Выходные данные:
+В единственную строку выходного файла OUTPUT.TXT нужно вывести одно целое число — сумму чисел А и В.
+
+Пример
+INPUT.TXT
+2 3
+OUTPUT.TXT
+5
+*/
+
+public class APlusB {
+    void solve(Scanner in, PrintWriter out) {
+        int firstNumber = in.nextInt();
+        int secondnumber = in.nextInt();
+        System.out.println(firstNumber + secondnumber);
+    }
+
+    void run() {
+
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        solve(in, out);
+    }
+
+    public static void main(String[] args) {
+        new APlusB().run();
+    }
+}
